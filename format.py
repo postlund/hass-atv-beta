fb2877a30b494f561cdf14ff87edd186f08ce09f
Pierre Ståhl <pierre.staahl@gmail.com>
2020-02-18 22:20:09 +0100

    Fix flake8 and black

--- format.py
+++ format.py
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-set -e
-
 DIRS="custom_components/apple_tv/ tests/apple_tv"
 
 for d in $DIRS
 do
-    black $d/*.py
-    isort $d/*.py
+    pushd $d
+    echo "* Formatting in $d"
+    black *.py
+    isort *.py
+    flake8 --max-line-length=88 --ignore=E501,W503,E203,D202,W504 *.py
+    popd
 done
