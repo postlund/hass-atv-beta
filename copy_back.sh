7a9812ff953e05fe577d1ae76a37bbf05dffc65d
Pierre Ståhl <pierre.staahl@gmail.com>
2021-10-27 18:42:14 +0200

    Bump to pyatv 0.9.6

--- copy_back.sh
+++ copy_back.sh
@@ -4,6 +4,7 @@ MANIFEST="../core/homeassistant/components/apple_tv/manifest.json"
 
 # Copy back files to core
 cp -r custom_components/apple_tv/* ../core/homeassistant/components/apple_tv
+cp -r custom_components/apple_tv/translations/* ../core/homeassistant/components/apple_tv/translations
 cp -r tests/apple_tv/* ../core/tests/components/apple_tv
 
 # Remove version from manifest
